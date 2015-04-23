eefeaea0ba617f0a2b0f0c677566ed7cef5b919c
juib <jui.b@coppermobile.com>
2015-04-22 22:13:02 -0400

    Added the ParallaxPagerTransformer

--- app/src/main/java/com/arvindjui/MainActivity.java
+++ app/src/main/java/com/arvindjui/MainActivity.java
@@ -1,38 +1,76 @@
 package com.arvindjui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+import com.xgc1986.parallaxPagerTransformer.ParallaxPagerTransformer;
 
+public class MainActivity extends FragmentActivity {
+
+    ViewPager mPager;
+    DemoParallaxAdapter mAdapter;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.demo_activity_parallax);
 
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setBackgroundColor(0xFF000000);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        ParallaxPagerTransformer pt = new ParallaxPagerTransformer((R.id.image));
+        pt.setBorder(20);
+        //pt.setSpeed(0.2f);
+        mPager.setPageTransformer(false, pt);
+
+        mAdapter = new DemoParallaxAdapter(getSupportFragmentManager());
+        mAdapter.setPager(mPager); //only for this transformer
+
+        Bundle bNina = new Bundle();
+        bNina.putInt("image", R.mipmap.bg_nina);
+        bNina.putString("name", "Nina");
+        DemoParallaxFragment pfNina = new DemoParallaxFragment();
+        pfNina.setArguments(bNina);
+
+        Bundle bNiju = new Bundle();
+        bNiju.putInt("image", R.mipmap.bg_niju);
+        bNiju.putString("name", "Ninu Junior");
+        DemoParallaxFragment pfNiju = new DemoParallaxFragment();
+        pfNiju.setArguments(bNiju);
+
+        Bundle bYuki = new Bundle();
+        bYuki.putInt("image", R.mipmap.bg_yuki);
+        bYuki.putString("name", "Yuki");
+        DemoParallaxFragment pfYuki = new DemoParallaxFragment();
+        pfYuki.setArguments(bYuki);
+
+        Bundle bKero = new Bundle();
+        bKero.putInt("image", R.mipmap.bg_kero);
+        bKero.putString("name", "Kero");
+        DemoParallaxFragment pfKero = new DemoParallaxFragment();
+        pfKero.setArguments(bKero);
+
+        mAdapter.add(pfNina);
+        mAdapter.add(pfNiju);
+        mAdapter.add(pfYuki);
+        mAdapter.add(pfKero);
+        mPager.setAdapter(mAdapter);
+
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().show();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        onBackPressed();
 
         return super.onOptionsItemSelected(item);
     }
